ebf7a67fc17dbe9c9717e9c9e7340a4a3bfc1ab8
christriwu <60164298+christriwu@users.noreply.github.com>
2023-11-12 23:35:49 -0500

    week 09 react app

---
+++ week09/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to Fullstack Development - I</h2>
+        <h3>React js Programming week09 Lab Exercise</h3>
+        <p>101335147</p>
+        <p>Chris Tri</p>
+        <p>George Brown College, Toronto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
